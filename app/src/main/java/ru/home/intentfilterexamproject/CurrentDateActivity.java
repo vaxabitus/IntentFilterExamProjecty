52ec54405b9938a845587ca71a27b7cce8675d33
vaxabitus <denislugovoj@yandex.ru>
2015-08-19 23:55:42 +0300

    The end of development App

--- app/src/main/java/ru/home/intentfilterexamproject/CurrentDateActivity.java
+++ app/src/main/java/ru/home/intentfilterexamproject/CurrentDateActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class CurrentDateActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class CurrentDateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_date);
+
+        TextView txtDate = (TextView) findViewById(R.id.txtDate);
+        txtDate.setText(Calendar.getInstance().getTime().toString());
+
+
     }
 }
--- app/src/main/java/ru/home/intentfilterexamproject/MainActivity.java
+++ app/src/main/java/ru/home/intentfilterexamproject/MainActivity.java
@@ -1,15 +1,43 @@
 package ru.home.intentfilterexamproject;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final String TAG = "Dev";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void explCall(View view){
+        Intent intent = new Intent(this,CurrentDateActivity.class);
+        startActivity(intent);
+
+        Log.i(TAG, "Current date view");
+    }
+
+
+    public void implCall(View view){
+        Intent intent = new Intent("home_site_act");
+        startActivity(intent);
+
+        Log.i(TAG,"implicity call Site activity");
+    }
+
+    public void openContacts(View view){
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.android.contacts","com.android.contacts.DialtactsContactsEntryActivity"));
+        startActivity(intent);
+
+        Log.i(TAG,"Open contacts");
     }
 }
